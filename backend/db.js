85f0129fc9d5c59b8288477cc0c0ffc66f6edbd9
IScodes777 <italodasilvasousa2008@gmail.com>
2025-02-15 21:55:28 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,33 +1,21 @@
+// db.js
 const mysql = require('mysql2');
-require('dotenv').config();
+
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+    host: 'localhost', // ou 'localhost'
+    user: 'Italo',
+    password: 'Informatica25',
+    database: 'gerenciamento_mercado_perto'
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar ao banco de dados', err.stack);
+    console.error('Erro ao conectar ao banco de dados:', err);
     return;
   }
-  console.log('Conectado ao banco de dados MySQL');
+  console.log('Conectado ao banco de dados!');
 });
 
-const query = async (sql) => {
-  return new Promise((resolve, reject) => {
-    connection.query(sql, (err, results) => {
-      if (err) {
-        console.error('Erro na consulta SQL', err.stack);
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    });
-  });
-};
+module.exports = connection;
 
-module.exports = { query };
